6cfee1db8664447d80f431bef839ba69c7d344ed
Mamedov Murad <mamedov_99b@mail.ru>
2022-07-15 12:33:42 +0300

    [FIX] demon

--- demon/main.py
+++ demon/main.py
@@ -1,6 +1,12 @@
 from app import Demon
+from src.models import create_db
+from config import logger
 
 
 if __name__ == '__main__':
     """Run script"""
+    try:
+        create_db()
+    except Exception as error:
+        logger.error(f"{error}")
     Demon().run()
