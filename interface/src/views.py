1b62a1f2176054c737afeb662fc1e98ec8c5c87f
Mamedov Murad <mamedov_99b@mail.ru>
2022-07-15 15:00:59 +0300

    [FIX] bugs

--- interface/src/views.py
+++ interface/src/views.py
@@ -1,4 +1,5 @@
 import json
+import math
 from flask import Blueprint, render_template
 
 from .settings import db
@@ -11,9 +12,15 @@ app = Blueprint("main", __name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index_page():
-    all_dates_usd, all_price_usd = get_statistic_data()
+    """Main page"""
+    orders = OrderModel.query.all()
+    all_dates_usd, all_price_usd = get_statistic_data(currency="USD")
+    all_dates_rub, all_price_rub = get_statistic_data(currency="RUB")
     return render_template("index.html", **{
+        "orders": orders,
         "all_price_usd": json.dumps(all_price_usd, default=str),
         "all_dates_usd": json.dumps(all_dates_usd),
-        "totalOrders": len(all_price_usd)
+        "total_orders": len(orders),
+        "total_orders_price_usd": round(math.fsum(all_price_usd), 3),
+        "total_orders_price_rub": round(math.fsum(all_price_rub), 3)
     })
